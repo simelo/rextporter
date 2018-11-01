a05bdc50b2b8b08d64f39c338af3059db62921d2
Alvaro Denis <denisacostaq@gmail.com>
2018-11-01 14:48:39 -0400

    introdce some const variables to refer to the metric type `string` value

--- src/config/metric.go
+++ src/config/metric.go
@@ -2,6 +2,17 @@ package config
 
 import "errors"
 
+const (
+	// KeyTypeCounter is the key you should define in the config file for counters.
+	KeyTypeCounter = "Counter"
+	// KeyTypeGauge is the key you should define in the config file for gauges.
+	KeyTypeGauge = "Gauge"
+	// KeyTypeHistogram is the key you should define in the config file for histograms.
+	KeyTypeHistogram = "Histogram"
+	// KeyTypeSummary is the key you should define in the config file for summaries.
+	KeyTypeSummary = "Summary"
+)
+
 // Metric keep the metric name as an instance of MetricOptions
 type Metric struct {
 	Name    string        `json:"name"`
