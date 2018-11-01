1d5af3b4dc78e67925ceed959d0beae1ba2694d2
Alvaro Denis <denisacostaq@gmail.com>
2018-11-01 14:42:26 -0400

    split `FindMetricType` in two new methods to get a new reusable `FindMetric` ref #3

--- src/config/link.go
+++ src/config/link.go
@@ -39,12 +39,22 @@ func (link Link) MetricDescription() (string, error) {
 func (link Link) FindMetricType() (metricType string, err error) {
 	const generalScopeErr = "error looking for metric type"
 	var metric Metric
-	if metric, err = Config().findMetricByRef(link.MetricRef); err != nil {
+	if metric, err = link.FindMetric(); err != nil {
 		errCause := fmt.Sprintln("can not find metric by ref: ", link.MetricRef, err.Error())
 		return metricType, common.ErrorFromThisScope(errCause, generalScopeErr)
 	}
 	metricType = metric.Options.Type
 	return metricType, err
+}
+
+// FindMetric will return the metric through the metric related with ref
+func (link Link) FindMetric() (metric Metric, err error) {
+	const generalScopeErr = "error looking for metric"
+	if metric, err = Config().findMetricByRef(link.MetricRef); err != nil {
+		errCause := fmt.Sprintln("can not find metric by ref: ", link.MetricRef, err.Error())
+		return metric, common.ErrorFromThisScope(errCause, generalScopeErr)
+	}
+	return metric, err
 }
 
 func (link Link) validate() (errs []error) {
