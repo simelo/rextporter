4e453bc111175d258f12628362956bc82dd1cf57
Alvaro Denis <denisacostaq@gmail.com>
2018-11-05 19:01:30 -0500

    be able to define the listen port ref #3

--- cmd/rextporter/main.go
+++ cmd/rextporter/main.go
@@ -12,8 +12,11 @@ func main() {
 	gopath := os.Getenv("GOPATH")
 	defaultConfigFilePath := filepath.Join(gopath, "", "src", "github.com", "simelo", "rextporter", "conf", "default", "skycoin.toml")
 	configFile := flag.String("config", defaultConfigFilePath, "Config file path.")
+	defaultListenPort := 8080
+	listenPort := flag.Uint("port", uint(defaultListenPort), "Listen port.")
 	flag.Parse()
-	exporter.ExportMetrics(*configFile, 8080)
+
+	exporter.ExportMetrics(*configFile, uint16(*listenPort))
 	waitForEver := make(chan bool)
 	<-waitForEver
 }
