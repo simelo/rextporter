cc8a5b58b21b5219f5a4282edc652f102be62217
Alvaro Denis <denisacostaq@gmail.com>
2018-11-10 15:22:27 -0500

    improving integration tests:  - check the health value of a metric name(by using metricName_up)  - add a test for service type proxy(checking the name concatenation) ref #18

--- test/integration/fake_skycoin_node.go
+++ test/integration/fake_skycoin_node.go
@@ -6,7 +6,153 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-var jsonResponse = `
+const exposedMetricsResponse = `
+# HELP go_gc_duration_seconds A summary of the GC invocation durations.
+# TYPE go_gc_duration_seconds summary
+go_gc_duration_seconds{quantile="0"} 0
+go_gc_duration_seconds{quantile="0.25"} 0
+go_gc_duration_seconds{quantile="0.5"} 0
+go_gc_duration_seconds{quantile="0.75"} 0
+go_gc_duration_seconds{quantile="1"} 0
+go_gc_duration_seconds_sum 0
+go_gc_duration_seconds_count 0
+# HELP go_goroutines Number of goroutines that currently exist.
+# TYPE go_goroutines gauge
+go_goroutines 7
+# HELP go_info Information about the Go environment.
+# TYPE go_info gauge
+go_info{version="go1.10.4"} 1
+# HELP go_memstats_alloc_bytes Number of bytes allocated and still in use.
+# TYPE go_memstats_alloc_bytes gauge
+go_memstats_alloc_bytes 1.060672e+06
+# HELP go_memstats_alloc_bytes_total Total number of bytes allocated, even if freed.
+# TYPE go_memstats_alloc_bytes_total counter
+go_memstats_alloc_bytes_total 1.060672e+06
+# HELP go_memstats_buck_hash_sys_bytes Number of bytes used by the profiling bucket hash table.
+# TYPE go_memstats_buck_hash_sys_bytes gauge
+go_memstats_buck_hash_sys_bytes 1.443364e+06
+# HELP go_memstats_frees_total Total number of frees.
+# TYPE go_memstats_frees_total counter
+go_memstats_frees_total 1007
+# HELP go_memstats_gc_cpu_fraction The fraction of this program's available CPU time used by the GC since the program started.
+# TYPE go_memstats_gc_cpu_fraction gauge
+go_memstats_gc_cpu_fraction 0
+# HELP go_memstats_gc_sys_bytes Number of bytes used for garbage collection system metadata.
+# TYPE go_memstats_gc_sys_bytes gauge
+go_memstats_gc_sys_bytes 169984
+# HELP go_memstats_heap_alloc_bytes Number of heap bytes allocated and still in use.
+# TYPE go_memstats_heap_alloc_bytes gauge
+go_memstats_heap_alloc_bytes 1.060672e+06
+# HELP go_memstats_heap_idle_bytes Number of heap bytes waiting to be used.
+# TYPE go_memstats_heap_idle_bytes gauge
+go_memstats_heap_idle_bytes 401408
+# HELP go_memstats_heap_inuse_bytes Number of heap bytes that are in use.
+# TYPE go_memstats_heap_inuse_bytes gauge
+go_memstats_heap_inuse_bytes 2.285568e+06
+# HELP go_memstats_heap_objects Number of allocated objects.
+# TYPE go_memstats_heap_objects gauge
+go_memstats_heap_objects 8042
+# HELP go_memstats_heap_released_bytes Number of heap bytes released to OS.
+# TYPE go_memstats_heap_released_bytes gauge
+go_memstats_heap_released_bytes 0
+# HELP go_memstats_heap_sys_bytes Number of heap bytes obtained from system.
+# TYPE go_memstats_heap_sys_bytes gauge
+go_memstats_heap_sys_bytes 2.686976e+06
+# HELP go_memstats_last_gc_time_seconds Number of seconds since 1970 of last garbage collection.
+# TYPE go_memstats_last_gc_time_seconds gauge
+go_memstats_last_gc_time_seconds 0
+# HELP go_memstats_lookups_total Total number of pointer lookups.
+# TYPE go_memstats_lookups_total counter
+go_memstats_lookups_total 15
+# HELP go_memstats_mallocs_total Total number of mallocs.
+# TYPE go_memstats_mallocs_total counter
+go_memstats_mallocs_total 9049
+# HELP go_memstats_mcache_inuse_bytes Number of bytes in use by mcache structures.
+# TYPE go_memstats_mcache_inuse_bytes gauge
+go_memstats_mcache_inuse_bytes 13888
+# HELP go_memstats_mcache_sys_bytes Number of bytes used for mcache structures obtained from system.
+# TYPE go_memstats_mcache_sys_bytes gauge
+go_memstats_mcache_sys_bytes 16384
+# HELP go_memstats_mspan_inuse_bytes Number of bytes in use by mspan structures.
+# TYPE go_memstats_mspan_inuse_bytes gauge
+go_memstats_mspan_inuse_bytes 31312
+# HELP go_memstats_mspan_sys_bytes Number of bytes used for mspan structures obtained from system.
+# TYPE go_memstats_mspan_sys_bytes gauge
+go_memstats_mspan_sys_bytes 32768
+# HELP go_memstats_next_gc_bytes Number of heap bytes when next garbage collection will take place.
+# TYPE go_memstats_next_gc_bytes gauge
+go_memstats_next_gc_bytes 4.473924e+06
+# HELP go_memstats_other_sys_bytes Number of bytes used for other system allocations.
+# TYPE go_memstats_other_sys_bytes gauge
+go_memstats_other_sys_bytes 1.05954e+06
+# HELP go_memstats_stack_inuse_bytes Number of bytes in use by the stack allocator.
+# TYPE go_memstats_stack_inuse_bytes gauge
+go_memstats_stack_inuse_bytes 458752
+# HELP go_memstats_stack_sys_bytes Number of bytes obtained from system for stack allocator.
+# TYPE go_memstats_stack_sys_bytes gauge
+go_memstats_stack_sys_bytes 458752
+# HELP go_memstats_sys_bytes Number of bytes obtained from system.
+# TYPE go_memstats_sys_bytes gauge
+go_memstats_sys_bytes 5.867768e+06
+# HELP go_threads Number of OS threads created.
+# TYPE go_threads gauge
+go_threads 7
+# HELP process_cpu_seconds_total Total user and system CPU time spent in seconds.
+# TYPE process_cpu_seconds_total counter
+process_cpu_seconds_total 0
+# HELP process_max_fds Maximum number of open file descriptors.
+# TYPE process_max_fds gauge
+process_max_fds 1024
+# HELP process_open_fds Number of open file descriptors.
+# TYPE process_open_fds gauge
+process_open_fds 9
+# HELP process_resident_memory_bytes Resident memory size in bytes.
+# TYPE process_resident_memory_bytes gauge
+process_resident_memory_bytes 1.2218368e+07
+# HELP process_start_time_seconds Start time of the process since unix epoch in seconds.
+# TYPE process_start_time_seconds gauge
+process_start_time_seconds 1.54180052506e+09
+# HELP process_virtual_memory_bytes Virtual memory size in bytes.
+# TYPE process_virtual_memory_bytes gauge
+process_virtual_memory_bytes 3.95628544e+08
+# HELP process_virtual_memory_max_bytes Maximum amount of virtual memory available in bytes.
+# TYPE process_virtual_memory_max_bytes gauge
+process_virtual_memory_max_bytes -1
+# HELP promhttp_metric_handler_requests_in_flight Current number of scrapes being served.
+# TYPE promhttp_metric_handler_requests_in_flight gauge
+promhttp_metric_handler_requests_in_flight 1
+# HELP promhttp_metric_handler_requests_total Total number of scrapes by HTTP status code.
+# TYPE promhttp_metric_handler_requests_total counter
+promhttp_metric_handler_requests_total{code="200"} 0
+promhttp_metric_handler_requests_total{code="500"} 0
+promhttp_metric_handler_requests_total{code="503"} 0
+# HELP skycoin_wallet1_seq1 I am running since
+# TYPE skycoin_wallet1_seq1 counter
+skycoin_wallet1_seq1 0
+# HELP skycoin_wallet1_seq1_up Says if the same name metric(skycoin_wallet1_seq1) was success updated, 1 for ok, 0 for failed.
+# TYPE skycoin_wallet1_seq1_up gauge
+skycoin_wallet1_seq1_up 1
+# HELP skycoin_wallet1_seq2 I am running since
+# TYPE skycoin_wallet1_seq2 counter
+skycoin_wallet1_seq2 0
+# HELP skycoin_wallet1_seq2_up Says if the same name metric(skycoin_wallet1_seq2) was success updated, 1 for ok, 0 for failed.
+# TYPE skycoin_wallet1_seq2_up gauge
+skycoin_wallet1_seq2_up 1
+# HELP skycoin_wallet2_seq1 I am running since
+# TYPE skycoin_wallet2_seq1 counter
+skycoin_wallet2_seq1 0
+# HELP skycoin_wallet2_seq1_up Says if the same name metric(skycoin_wallet2_seq1) was success updated, 1 for ok, 0 for failed.
+# TYPE skycoin_wallet2_seq1_up gauge
+skycoin_wallet2_seq1_up 1
+# HELP skycoin_wallet2_seq2 I am running since
+# TYPE skycoin_wallet2_seq2 counter
+skycoin_wallet2_seq2 0
+# HELP skycoin_wallet2_seq2_up Says if the same name metric(skycoin_wallet2_seq2) was success updated, 1 for ok, 0 for failed.
+# TYPE skycoin_wallet2_seq2_up gauge
+skycoin_wallet2_seq2_up 0
+`
+
+const jsonHealthResponse = `
 {
     "blockchain": {
         "head": {
@@ -39,14 +185,24 @@ var jsonResponse = `
 }
 `
 
-func httpHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	if _, err := w.Write([]byte(jsonResponse)); err != nil {
+func apiHealthHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("r.RequestURI\n\n", r.RequestURI)
+	if _, err := w.Write([]byte(jsonHealthResponse)); err != nil {
 		log.WithError(err).Panicln("unable to write response")
 	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func exposedMetricHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("r.RequestURI\n\n", r.RequestURI)
+	if _, err := w.Write([]byte(exposedMetricsResponse)); err != nil {
+		log.WithError(err).Panicln("unable to write response")
+	}
+	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
-	handler := http.HandlerFunc(httpHandler)
-	log.WithError(http.ListenAndServe(":8080", handler)).Fatalln("server fail")
+	http.HandleFunc("/api/v1/health", apiHealthHandler)
+	http.HandleFunc("/metrics", exposedMetricHandler)
+	log.WithError(http.ListenAndServe(":8080", nil)).Fatalln("server fail")
 }
--- test/integration/integration_test.go
+++ test/integration/integration_test.go
@@ -3,20 +3,52 @@ package main
 import (
 	"context"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"testing"
 	"time"
 
 	"github.com/alecthomas/template"
 	"github.com/simelo/rextporter/src/common"
+	"github.com/simelo/rextporter/src/config"
 	"github.com/simelo/rextporter/src/exporter"
+	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 )
+
+const serviceApiRestConfigFileContenTemplate = `
+	# Service configuration.
+	[[services]]
+		name = "myMonitoredServer"
+		mode = "apiRest"
+		scheme = "http"
+		port = 8080
+		basePath = ""
+		authType = "CSRF"
+		tokenHeaderKey = "X-CSRF-Token"
+		genTokenEndpoint = "/api/v1/csrf"
+		tokenKeyFromEndpoint = "csrf_token"
+		
+		[services.location]
+			location = "localhost"
+`
+
+const serviceProxyConfigFileContenTemplate = `
+	# Service configuration.
+	[[services]]
+		name = "myMonitoredServer"
+		mode = "proxy"
+		scheme = "http"
+		port = 8080
+		basePath = "/metrics"
+		
+		[services.location]
+			location = "localhost"
+`
 
 func createConfigFile(tmplContent, path string, data interface{}) (err error) {
 	generalScopeErr := "error creating config file for integration test"
@@ -50,24 +82,8 @@ func createServiceConfig(tmplContent, path string) (err error) {
 	return err
 }
 
-func createServiceConfigPaths(serviceConfigPath string) (err error) {
-	const serviceConfigFileContenTemplate = `
-	# Service configuration.
-	[[services]]
-		name = "myMonitoredServer"
-		mode = "apiRest"
-		scheme = "http"
-		port = 8080
-		basePath = ""
-		authType = "CSRF"
-		tokenHeaderKey = "X-CSRF-Token"
-		genTokenEndpoint = "/api/v1/csrf"
-		tokenKeyFromEndpoint = "csrf_token"
-		
-		[services.location]
-			location = "localhost"
-`
-	return createServiceConfig(serviceConfigFileContenTemplate, serviceConfigPath)
+func createServiceConfigPaths(serviceConfigPath, serviceContent string) (err error) {
+	return createServiceConfig(serviceContent, serviceConfigPath)
 }
 
 func createMetricsConfig(tmplContent, path string) (err error) {
@@ -95,7 +111,7 @@ func createMetricsConfigPaths(metricsConfigPath string) (err error) {
 	return createMetricsConfig(metricsConfigFileContenTemplate, metricsConfigPath)
 }
 
-func createMainConfig(tmplContent, path, metricsConfigPath, serviceConfigPath string) (err error) {
+func createMainConfig(tmplContent, path, metricsConfigPath, serviceConfigPath, serviceContent string) (err error) {
 	generalScopeErr := "error creating main config file for integration test"
 	type mainConfigData struct {
 		ServiceConfigPath string
@@ -109,7 +125,7 @@ func createMainConfig(tmplContent, path, metricsConfigPath, serviceConfigPath st
 		errCause := "error writing main config file: " + err.Error()
 		return common.ErrorFromThisScope(errCause, generalScopeErr)
 	}
-	if err = createServiceConfigPaths(serviceConfigPath); err != nil {
+	if err = createServiceConfigPaths(serviceConfigPath, serviceContent); err != nil {
 		errCause := "error writing service config file: " + err.Error()
 		return common.ErrorFromThisScope(errCause, generalScopeErr)
 	}
@@ -120,7 +136,7 @@ func createMainConfig(tmplContent, path, metricsConfigPath, serviceConfigPath st
 	return err
 }
 
-func createMainConfigTestPaths() (mainConfFilePath string, err error) {
+func createMainConfigTestPaths(serviceContent string) (mainConfFilePath string, err error) {
 	const mainConfigFileContenTemplate = `
 serviceConfigTransport = "file"
 # render a template with a portable path
@@ -143,10 +159,10 @@ metricsConfigPath = "{{.MetricsConfigPath}}"
 	}
 	metricsConfigPath := filepath.Join(metricsDir, "metrics.toml")
 	return mainConfFilePath,
-		createMainConfig(mainConfigFileContenTemplate, mainConfFilePath, metricsConfigPath, serviceConfigPath)
+		createMainConfig(mainConfigFileContenTemplate, mainConfFilePath, metricsConfigPath, serviceConfigPath, serviceContent)
 }
 
-func createMainConfigCustomPaths() (mainConfFilePath string, err error) {
+func createMainConfigCustomPaths(serviceContent string) (mainConfFilePath string, err error) {
 	const mainConfigFileContenTemplate = `
 serviceConfigTransport = "file"
 # render a template with a portable path
@@ -160,7 +176,7 @@ metricsConfigPath = "{{.MetricsConfigPath}}"
 	mainConfFilePath = filepath.Join(mainConfigDir, "rrrr")
 	serviceConfigPath := filepath.Join(os.TempDir(), "sdsds", "epe.toml")
 	return mainConfFilePath,
-		createMainConfig(mainConfigFileContenTemplate, mainConfFilePath, "", serviceConfigPath)
+		createMainConfig(mainConfigFileContenTemplate, mainConfFilePath, "", serviceConfigPath, serviceContent)
 }
 
 type HealthSuit struct {
@@ -171,13 +187,57 @@ func TestSkycoinHealthSuit(t *testing.T) {
 	suite.Run(t, new(HealthSuit))
 }
 
+func metricHealthIsOk(metricName, metricData string) bool {
+	if !strings.Contains(metricData, metricName) {
+		log.WithField("metricName", metricName).Errorln("metric name not found")
+		return false
+	}
+	metricHealth := metricName + "_up"
+	if !strings.Contains(metricData, metricHealth) {
+		log.WithField("metricHealth", metricHealth).Errorln("metric health name not found")
+		return false
+	}
+	lines := strings.Split(metricData, "\n")
+	var linesWhoMentionMetric []string
+	for _, line := range lines {
+		if strings.Contains(line, metricHealth) {
+			linesWhoMentionMetric = append(linesWhoMentionMetric, line)
+		}
+	}
+	var targetLine string
+	for _, line := range linesWhoMentionMetric {
+		if strings.Contains(line, "# TYPE ") || strings.Contains(line, "# HELP ") {
+			continue
+		} else {
+			targetLine = line
+			break
+		}
+	}
+	if strings.Compare(targetLine, "") == 0 {
+		log.Errorln("can not find target line")
+		return false
+	}
+	targetFields := strings.Split(targetLine, " ")
+	if val, err := strconv.Atoi(targetFields[1]); err != nil || val != 0 {
+		if err != nil {
+			log.WithError(err).Errorln("unable to convert the value")
+		}
+		if val != 0 {
+			log.WithField("val", val).Errorln("flag is set")
+		}
+		return false
+	}
+	return true
+}
+
 func (suite *HealthSuit) TestMetricMonitorHealth() {
 	// NOTE(denisacostaq@gmail.com): Giving
 	require := require.New(suite.T())
-	mainConfFilePath, err := createMainConfigTestPaths()
+	mainConfFilePath, err := createMainConfigTestPaths(serviceApiRestConfigFileContenTemplate)
 	require.Nil(err)
 	srv := exporter.ExportMetrics(mainConfFilePath, "/metrics", 8081)
 	require.NotNil(srv)
+	conf := config.Config()
 	// NOTE(denisacostaq@gmail.com): Wait for server starts
 	t := time.NewTimer(time.Second * 2)
 	<-t.C
@@ -188,11 +248,47 @@ func (suite *HealthSuit) TestMetricMonitorHealth() {
 
 	// NOTE(denisacostaq@gmail.com): Assert
 	suite.Nil(err)
+	suite.Equal(http.StatusOK, resp.StatusCode)
 	var data []byte
 	data, err = ioutil.ReadAll(resp.Body)
 	suite.Nil(err)
+	require.Len(conf.Services, 1)
+	require.Len(conf.Metrics, 1)
+	metricName := "skycoin_" + conf.Services[0].Name + "_" + conf.Metrics[0].Name
+	require.Equal(metricName, "skycoin_myMonitoredServer_open_connections_is_a_fake_name_for_test_purpose")
+	require.True(metricHealthIsOk(metricName, string(data)))
+	var usingAVariableToMakeLinterHappy = context.Context(nil)
+	require.Nil(srv.Shutdown(usingAVariableToMakeLinterHappy))
+}
+
+func (suite *HealthSuit) TestMetricMonitorAsProxy() {
+	// NOTE(denisacostaq@gmail.com): Giving
+	require := require.New(suite.T())
+	mainConfFilePath, err := createMainConfigCustomPaths(serviceProxyConfigFileContenTemplate)
+	require.Nil(err)
+	log.Errorln("mainConfFilePath", mainConfFilePath)
+	srv := exporter.ExportMetrics(mainConfFilePath, "/metrics3", 8082)
+	require.NotNil(srv)
+	conf := config.Config()
+	log.Errorln("services2", len(conf.Services))
+	// NOTE(denisacostaq@gmail.com): Wait for server starts
+	time.Sleep(time.Second * 2)
+
+	// NOTE(denisacostaq@gmail.com): When
+	var resp *http.Response
+	resp, err = http.Get("http://127.0.0.1:8082/metrics3")
+
+	// NOTE(denisacostaq@gmail.com): Assert
+	suite.Nil(err)
 	suite.Equal(http.StatusOK, resp.StatusCode)
-	suite.Contains(string(data), "open_connections_is_a_fake_name_for_test_purpose")
+	var data []byte
+	data, err = ioutil.ReadAll(resp.Body)
+	suite.Nil(err)
+	require.Len(conf.Services, 1)
+	require.Len(conf.Metrics, 1)
+	metricName := conf.Services[0].Name + "_skycoin_wallet2_seq2"
+	require.Equal(metricName, "myMonitoredServer_skycoin_wallet2_seq2")
+	require.True(metricHealthIsOk(metricName, string(data)))
 	var usingAVariableToMakeLinterHappy = context.Context(nil)
 	require.Nil(srv.Shutdown(usingAVariableToMakeLinterHappy))
 }
@@ -200,21 +296,31 @@ func (suite *HealthSuit) TestMetricMonitorHealth() {
 func (suite *HealthSuit) TestConfigWorks() {
 	// NOTE(denisacostaq@gmail.com): Giving
 	require := require.New(suite.T())
-	mainConfFilePath, err := createMainConfigCustomPaths()
+	mainConfFilePath, err := createMainConfigCustomPaths(serviceApiRestConfigFileContenTemplate)
 	require.Nil(err)
+	log.Errorln("mainConfFilePath", mainConfFilePath)
 	srv := exporter.ExportMetrics(mainConfFilePath, "/metrics2", 8082)
 	require.NotNil(srv)
+	conf := config.Config()
+	log.Errorln("services2", len(conf.Services))
 	// NOTE(denisacostaq@gmail.com): Wait for server starts
-	t := time.NewTimer(time.Second * 2)
-	<-t.C
+	time.Sleep(time.Second * 2)
 
-	// // NOTE(denisacostaq@gmail.com): When
+	// NOTE(denisacostaq@gmail.com): When
 	var resp *http.Response
 	resp, err = http.Get("http://127.0.0.1:8082/metrics2")
-	log.Println("resp, err", resp, err)
-	// // NOTE(denisacostaq@gmail.com): Assert
+
+	// NOTE(denisacostaq@gmail.com): Assert
 	suite.Nil(err)
 	suite.Equal(http.StatusOK, resp.StatusCode)
+	var data []byte
+	data, err = ioutil.ReadAll(resp.Body)
+	suite.Nil(err)
+	require.Len(conf.Services, 1)
+	require.Len(conf.Metrics, 1)
+	metricName := "skycoin_" + conf.Services[0].Name + "_" + conf.Metrics[0].Name
+	require.Equal(metricName, "skycoin_myMonitoredServer_seq")
+	require.True(metricHealthIsOk(metricName, string(data)))
 	var usingAVariableToMakeLinterHappy = context.Context(nil)
 	require.Nil(srv.Shutdown(usingAVariableToMakeLinterHappy))
 }
