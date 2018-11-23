61d1c22fa95de4e14e1ed59690810a4596adfb93
Alvaro Denis <denisacostaq@gmail.com>
2018-11-23 12:05:24 -0500

    ref #44 define a scrapper interface

--- src/scrapper/scrapper.go
+++ src/scrapper/scrapper.go
@@ -2,5 +2,5 @@ package scrapper
 
 // Scrapper receive some data as input and should return the metric val
 type Scrapper interface {
-	GetMetric(data interface{}) interface{}
+	GetMetric() (val interface{}, err error)
 }
