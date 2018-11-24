dc6283e8cf4758f7c9aaed40a6a2caa577ffbefd
Olemis Lang <olemis+work@gmail.com>
2018-11-23 19:00:57 -0500

    [config] [dsl] refs #32 - RXT AST data types

---
+++ src/config/rxt/ast.go
@@ -0,0 +1,43 @@
+package rxt
+
+import "github.com/simelo/rextporter/src/core"
+
+type OptionsMap map[string]string
+
+func newOptionsMap() OptionsMap {
+
+}
+
+type RxtDefScraperDataset struct {
+	SupportedServiceNames []string
+	SupportedStackNames   []string
+	Definitions           map[string]interface{}
+	Sources               []RxtDefDataSource
+	Options               OptionsMap
+}
+
+type RxtDefAuth struct {
+	options OptionMap
+}
+
+type RxtDefSource struct {
+	Method   string
+	Type     string
+	Location string
+	Scrapers RxtDefScraper
+	Options  OptionsMap
+}
+
+type RxtDefScraper struct {
+	Type    string
+	Metrics []RxtDefMetric
+	Options OptionsMap
+}
+
+type RxtDefMetric struct {
+	Type        string
+	Name        string
+	Description string
+	Labels      []string
+	Options     OptionsMap
+}
