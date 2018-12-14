a3d9d4312d3b0034672c9ba2799ef5415b357269
Alvaro Denis <dev0003@simelo.tech>
2018-12-14 11:27:36 -0500

    ref #32 implements core.RextKeyValStore (copied from config module, olemis implementation)

---
+++ src/memconfig/storage.go
@@ -0,0 +1,60 @@
+package memconfig
+
+import (
+	"github.com/simelo/rextporter/src/core"
+)
+
+// OptionsMap in-memory key value store
+type OptionsMap map[string]string
+
+// NewOptionsMap creates a new instance
+func NewOptionsMap() (m OptionsMap) {
+	m = make(OptionsMap)
+	return
+}
+
+// GetString value for key
+func (m OptionsMap) GetString(key string) (string, error) {
+	if val, hasKey := m[key]; hasKey {
+		return val, nil
+	}
+	return "", core.ErrKeyNotFound
+}
+
+// SetString value for key
+func (m OptionsMap) SetString(key string, value string) (exists bool, err error) {
+	err = nil
+	_, exists = m[key]
+	m[key] = value
+	return
+}
+
+// GetObject equivalent to GetString
+func (m OptionsMap) GetObject(key string) (interface{}, error) {
+	return m.GetString(key)
+}
+
+// SetObject only strings supported
+func (m OptionsMap) SetObject(key string, value interface{}) (bool, error) {
+	if s, isStr := value.(string); isStr {
+		return m.SetString(key, s)
+	}
+	return false, core.ErrInvalidType
+}
+
+// GetKeys return all the saved keys
+func (m OptionsMap) GetKeys() (keys []string) {
+	for k := range m {
+		keys = append(keys, k)
+	}
+	return
+}
+
+// Clone make a deep copy of the storage
+func (m OptionsMap) Clone() (core.RextKeyValueStore, error) {
+	clone := NewOptionsMap()
+	for k := range m {
+		clone[k] = m[k]
+	}
+	return clone, nil
+}
