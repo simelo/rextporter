511c63b72d56ee5bf5ea5f2f97305eda91f44de8
Alvaro Denis <dev0003@simelo.tech>
2018-12-19 00:29:35 -0500

    ref #32 auth implementation use an KeyValueStore implementation as parameter

--- src/memconfig/http_auth.go
+++ src/memconfig/http_auth.go
@@ -2,13 +2,18 @@ package memconfig
 
 import (
 	"github.com/simelo/rextporter/src/core"
-	log "github.com/sirupsen/logrus"
 )
 
 // HTTPAuth implements the core.RextAuth interface
 type HTTPAuth struct {
 	authType string
 	endpoint string
+	options  core.RextKeyValueStore
+}
+
+// SetAuthType return the auth type
+func (auth *HTTPAuth) SetAuthType(authType string) {
+	auth.authType = authType
 }
 
 // GetAuthType return the auth type
@@ -17,99 +22,18 @@ func (auth HTTPAuth) GetAuthType() string {
 }
 
 // GetOptions return key/value pairs for extra options
-func (auth HTTPAuth) GetOptions() core.RextKeyValueStore {
-	return &HTTPAuthKeyValueConf{httpAuth: auth}
-}
-
-// HTTPAuthKeyValueConf allow help to implement the core.RextKeyValueStore for http auth
-type HTTPAuthKeyValueConf struct {
-	httpAuth HTTPAuth
-}
-
-const (
-	// OptionsKeyAuthEndpoint key used to save auth endpoint info
-	OptionsKeyAuthEndpoint = "e84d8928-c074-4f19-897e-84fdaf61520a"
-)
-
-// GetString return a string value for a giving key
-func (kv HTTPAuthKeyValueConf) GetString(key string) (val string, err error) {
-	switch key {
-	case OptionsKeyAuthEndpoint:
-		val = kv.httpAuth.endpoint
-	default:
-		err = core.ErrKeyNotFound
-		log.WithField("key", key).Errorln(err.Error())
+func (auth *HTTPAuth) GetOptions() core.RextKeyValueStore {
+	if auth.options == nil {
+		auth.options = NewOptionsMap()
 	}
-	return val, err
-}
-
-// SetString set a string value for a giving key
-func (kv *HTTPAuthKeyValueConf) SetString(key, value string) (alreadyExist bool, err error) {
-	alreadyExist = false
-	if val, err := kv.GetString(key); err != nil && len(val) != 0 {
-		alreadyExist = true
-	}
-	switch key {
-	case OptionsKeyAuthEndpoint:
-		kv.httpAuth.endpoint = value
-	default:
-		err = core.ErrKeyNotFound
-		log.WithField("key", key).Errorln(err.Error())
-	}
-	return alreadyExist, err
-}
-
-// GetObject return an interface{} value for a giving key
-func (kv HTTPAuthKeyValueConf) GetObject(key string) (val interface{}, err error) {
-	switch key {
-	case OptionsKeyAuthEndpoint:
-		val = kv.httpAuth.endpoint
-	default:
-		err = core.ErrKeyNotFound
-		log.WithField("key", key).Errorln(err.Error())
-	}
-	return nil, err
-}
-
-// SetObject set an interface{} value for a giving key
-func (kv *HTTPAuthKeyValueConf) SetObject(key string, value interface{}) (alreadyExist bool, err error) {
-	alreadyExist = false
-	if val, err := kv.GetObject(key); err != nil && val != nil {
-		alreadyExist = true
-	}
-	switch key {
-	case OptionsKeyAuthEndpoint:
-		strVal, okStrVal := value.(string)
-		if okStrVal {
-			kv.httpAuth.endpoint = strVal
-		} else {
-			log.WithField("val", value).Errorln("value not convertibe to string")
-			err = core.ErrInvalidType
-		}
-	default:
-		err = core.ErrKeyNotFound
-		log.WithField("key", key).Errorln(err.Error())
-	}
-	return alreadyExist, err
-}
-
-// GetKeys return all the valid key for this storage
-// FIXME(denisacostaq@gmail.com): This is a bug, no all keys have to be present all the time, the
-// olemis implementation is better about this
-func (kv HTTPAuthKeyValueConf) GetKeys() []string {
-	return []string{OptionsKeyAuthEndpoint}
-}
-
-// Clone make a copy of HTTPAuthKeyValueConf, including the internal service
-func (kv *HTTPAuthKeyValueConf) Clone() (core.RextKeyValueStore, error) {
-	retKv := *kv
-	return &retKv, nil
+	return auth.options
 }
 
 // NewHTTPAuth create a auth
-func NewHTTPAuth(aType, url string, options core.RextKeyValueStore) *HTTPAuth {
+func NewHTTPAuth(aType, url string, options core.RextKeyValueStore) core.RextAuthDef {
 	return &HTTPAuth{
 		authType: aType,
 		endpoint: url,
+		options:  options,
 	}
 }
