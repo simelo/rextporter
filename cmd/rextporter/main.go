e3108b5f4a17cee9fd20a16186ce9b94ab0b1571
Alvaro Denis <dev0003@simelo.tech>
2018-12-18 23:36:54 -0500

    ref #32 reading toml config from cmd/rextporter

--- cmd/rextporter/main.go
+++ cmd/rextporter/main.go
@@ -3,19 +3,25 @@ package main
 import (
 	"flag"
 
-	"github.com/simelo/rextporter/src/config"
 	"github.com/simelo/rextporter/src/exporter"
+	"github.com/simelo/rextporter/src/toml2config"
+	"github.com/simelo/rextporter/src/tomlconfig"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	mainConfigFile := flag.String("config", "", "Metrics main config file path.")
+	// log.SetFlags(log.LstdFlags | log.Lshortfile)
+	mainConfigFile := flag.String("config", "/home/adacosta/.config/simelo/rextporter/main.toml", "Metrics main config file path.")
 	defaultListenPort := 8080
 	listenPort := flag.Uint("port", uint(defaultListenPort), "Listen port.")
 	defaultHandlerEndpoint := "/metrics"
 	handlerEndpoint := flag.String("handler", defaultHandlerEndpoint, "Handler endpoint.")
 	flag.Parse()
-	conf := config.MustConfigFromFileSystem(*mainConfigFile)
-	exporter.MustExportMetrics(*handlerEndpoint, uint16(*listenPort), conf)
+	conf, err := tomlconfig.ReadConfigFromFileSystem(*mainConfigFile)
+	if err != nil {
+		log.WithError(err).Errorln("error reading config from file system")
+	}
+	exporter.MustExportMetrics(*handlerEndpoint, uint16(*listenPort), toml2config.Fill(conf))
 	waitForEver := make(chan bool)
 	<-waitForEver
 }
