bf54347e0498041ee09e92e09ecabe72db790600
Alvaro Denis <dev0003@simelo.tech>
2018-12-28 15:19:58 -0500

    ref #66 add integration test to skycoin health -> /blockchain/head/seq

---
+++ test/integration/skycoin/integration_test.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/denisacostaq/rextporter/test/util"
+	"github.com/simelo/rextporter/src/core"
+	"github.com/simelo/rextporter/src/exporter"
+	"github.com/simelo/rextporter/src/toml2config"
+	"github.com/simelo/rextporter/src/tomlconfig"
+	"github.com/simelo/rextporter/test/util/testrand"
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type SkycoinSuit struct {
+	suite.Suite
+	require            *require.Assertions
+	rextporterEndpoint string
+	rextporterServer   *http.Server
+}
+
+func (suite *SkycoinSuit) SetupSuite() {
+	suite.require = require.New(suite.T())
+	mainConfFilePath := "/usr/share/gocode/src/github.com/simelo/rextporter/test/integration/skycoin/tomlconfig/main.toml"
+	tomlConf, err := tomlconfig.ReadConfigFromFileSystem(mainConfFilePath)
+	suite.Nil(err)
+	var conf core.RextRoot
+	conf, err = toml2config.Fill(tomlConf)
+	suite.Nil(err)
+	listenPort := testrand.RandomPort()
+	suite.rextporterEndpoint = fmt.Sprintf("http://localhost:%d%s", listenPort, "/metrics")
+	suite.rextporterServer = exporter.MustExportMetrics("", "/metrics", listenPort, conf)
+	suite.require.NotNil(suite.rextporterServer)
+	// NOTE(denisacostaq@gmail.com): Wait for server starts
+	time.Sleep(time.Second * 2)
+}
+
+func (suite *SkycoinSuit) TearDownSuite() {
+	log.Info("Shutting down server...")
+	suite.Nil(suite.rextporterServer.Shutdown(context.Context(nil)))
+}
+
+func TestSkycoinSuitSuit(t *testing.T) {
+	suite.Run(t, new(SkycoinSuit))
+}
+
+func (suite *SkycoinSuit) TestMetricsAreExported() {
+	// NOTE(denisacostaq@gmail.com): Giving
+
+	// NOTE(denisacostaq@gmail.com): When
+	resp, err := http.Get(suite.rextporterEndpoint)
+
+	// NOTE(denisacostaq@gmail.com): Assert
+	suite.Nil(err)
+	suite.Equal(http.StatusOK, resp.StatusCode)
+	suite.NotNil(resp.Body)
+}
+
+func (suite *SkycoinSuit) TestDefaultMetricsForAPIScrapperArePresent() {
+	// NOTE(denisacostaq@gmail.com): Giving
+
+	// NOTE(denisacostaq@gmail.com): When
+	resp, err := http.Get(suite.rextporterEndpoint)
+
+	// NOTE(denisacostaq@gmail.com): Assert
+	suite.Nil(err)
+	suite.Equal(http.StatusOK, resp.StatusCode)
+	suite.NotNil(resp.Body)
+	var respBody []byte
+	respBody, err = ioutil.ReadAll(resp.Body)
+	suite.Nil(err)
+	suite.NotNil(respBody)
+	mtrs := []string{
+		"scrape_duration_seconds",
+		"scrape_samples_scraped",
+		"data_source_response_duration_seconds",
+		"data_source_scrape_duration_seconds",
+		"data_source_scrape_samples_scraped",
+	}
+	for _, mtr := range mtrs {
+		var found bool
+		found, err = util.FoundMetric(respBody, mtr)
+		suite.Nil(err)
+		suite.True(found)
+	}
+	var found bool
+	found, err = util.FoundMetric(respBody, "data_source_scrape_samples_scrapeds")
+	suite.Nil(err)
+	suite.False(found)
+}
+
+func (suite *SkycoinSuit) TestDefaultMetricsForMetricsFordwaderArePresent() {
+	// NOTE(denisacostaq@gmail.com): Giving
+
+	// NOTE(denisacostaq@gmail.com): When
+	resp, err := http.Get(suite.rextporterEndpoint)
+
+	// NOTE(denisacostaq@gmail.com): Assert
+	suite.Nil(err)
+	suite.Equal(http.StatusOK, resp.StatusCode)
+	suite.NotNil(resp.Body)
+	var respBody []byte
+	respBody, err = ioutil.ReadAll(resp.Body)
+	suite.Nil(err)
+	suite.NotNil(respBody)
+	mtrs := []string{
+		"fordwader_response_duration_seconds",
+		"fordwader_scrape_duration_seconds",
+	}
+	for _, mtr := range mtrs {
+		var found bool
+		found, err = util.FoundMetric(respBody, mtr)
+		suite.Nil(err)
+		suite.True(found)
+	}
+	var found bool
+	found, err = util.FoundMetric(respBody, "fordwader_scrape_duration_secondss")
+	suite.Nil(err)
+	suite.False(found)
+}
+
+func (suite *SkycoinSuit) TestHealthSeq() {
+	// NOTE(denisacostaq@gmail.com): Giving
+
+	// NOTE(denisacostaq@gmail.com): When
+	resp, err := http.Get(suite.rextporterEndpoint)
+
+	// NOTE(denisacostaq@gmail.com): Assert
+	suite.Nil(err)
+	suite.Equal(http.StatusOK, resp.StatusCode)
+	suite.NotNil(resp.Body)
+	var respBody []byte
+	respBody, err = ioutil.ReadAll(resp.Body)
+	suite.Nil(err)
+	suite.NotNil(respBody)
+	var found bool
+	found, err = util.FoundMetric(respBody, "seq")
+	suite.Nil(err)
+	suite.True(found)
+	var val float64
+	val, err = util.GetCounterValue(respBody, "seq")
+	suite.Nil(err)
+	suite.Equal(float64(180), val)
+}
--- test/util/metrics_endpoint.go
+++ test/util/metrics_endpoint.go
@@ -46,3 +46,24 @@ func GetGaugeValue(metrics []byte, mtrName string) (float64, error) {
 	}
 	return -1, core.ErrKeyNotFound
 }
+
+// GetCounterValue return a number(the counter value) if found a metric with mtrName and if a Counter kind metric
+// from a metrics endpoint response as plain text.
+func GetCounterValue(metrics []byte, mtrName string) (float64, error) {
+	var parser expfmt.TextParser
+	in := bytes.NewReader(metrics)
+	metricFamilies, err := parser.TextToMetricFamilies(in)
+	if err != nil {
+		log.WithError(err).Errorln("error, reading text format failed")
+		return -1, core.ErrKeyDecodingFile
+	}
+	for _, mf := range metricFamilies {
+		if mtrName == *mf.Name {
+			if (*mf.Type).String() != strings.ToUpper(core.KeyMetricTypeCounter) {
+				return -1, core.ErrKeyInvalidType
+			}
+			return *mf.Metric[0].Counter.Value, nil
+		}
+	}
+	return -1, core.ErrKeyNotFound
+}
