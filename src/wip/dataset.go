e43f22326396ce1bd796ead693ed3e1fc772d9ce
Alvaro Denis <dev0003@simelo.tech>
2019-01-03 12:08:47 -0500

    ref #66 use RextMetricDef from core

--- src/wip/dataset.go
+++ src/wip/dataset.go
@@ -1,7 +1,9 @@
 package wip
 
+import "github.com/simelo/rextporter/src/core"
+
 // RextMetric provides access to values measured for a given metric
 type RextMetric interface {
-	GetMetadata() RextMetricDef
+	GetMetadata() core.RextMetricDef
 	// TODO: Methods to retrieve values measured for a given metric
 }
